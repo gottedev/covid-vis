110c581d0c870f71dd777cfe2873d542a62ca426
gottedev <gottedev@gmail.com>
2020-08-19 14:00:02 +0100

    Updates

---
+++ precache-manifest.263bf4ff0185567d73f40849e6e494db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2cdce262a74ce534f787649ce3d2455",
+    "url": "/covid-vis/index.html"
+  },
+  {
+    "revision": "59924d7da32b0ede1ffd",
+    "url": "/covid-vis/static/css/2.19b7558e.chunk.css"
+  },
+  {
+    "revision": "2d2d752393c8cedb2920",
+    "url": "/covid-vis/static/css/main.a0e34a6a.chunk.css"
+  },
+  {
+    "revision": "59924d7da32b0ede1ffd",
+    "url": "/covid-vis/static/js/2.de441229.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/covid-vis/static/js/2.de441229.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d2d752393c8cedb2920",
+    "url": "/covid-vis/static/js/main.0e63b78e.chunk.js"
+  },
+  {
+    "revision": "b0877440a803dbf75371",
+    "url": "/covid-vis/static/js/runtime-main.e86cca98.js"
+  }
+]);
