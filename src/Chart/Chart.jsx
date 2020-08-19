a731712cd086ba99b2637f65f62f79596493007a
gottedev <gottedev@gmail.com>
2020-08-19 01:06:46 +0100

    removes context api usage

--- src/Chart/Chart.jsx
+++ src/Chart/Chart.jsx
@@ -1,29 +1,22 @@
-import React, { createContext, useContext } from "react";
+import React from "react";
 import { dimensionsPropsType } from "./utils";
 
 import "./Chart.css";
 
-const ChartContext = createContext();
-export const useChartDimensions = () => useContext(ChartContext);
-
-const Chart = ({ dimensions, children }) => {
-  return (
-    <ChartContext.Provider value={dimensions}>
-      <svg
-        className="Chart"
-        width={dimensions.width}
-        height={dimensions.height}
-        viewBox={`0 0 ${dimensions.width} ${dimensions.height}`}
-      >
-        <g
-          transform={`translate(${dimensions.marginLeft}, ${dimensions.marginTop})`}
-        >
-          {children}
-        </g>
-      </svg>
-    </ChartContext.Provider>
-  );
-};
+const Chart = ({ dimensions, children }) => (
+  <svg
+    className="Chart"
+    width={dimensions.width}
+    height={dimensions.height}
+    viewBox={`0 0 ${dimensions.width} ${dimensions.height}`}
+  >
+    <g
+      transform={`translate(${dimensions.marginLeft}, ${dimensions.marginTop})`}
+    >
+      {children}
+    </g>
+  </svg>
+);
 
 Chart.propTypes = {
   dimensions: dimensionsPropsType,
