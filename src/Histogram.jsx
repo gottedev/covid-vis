2b39f77e87af1ebeb17ed65d55645b8a889318ee
gottedev <gottedev@gmail.com>
2020-08-24 15:19:58 +0100

    refactor

--- src/Histogram.jsx
+++ src/Histogram.jsx
@@ -1,12 +1,10 @@
-import React, { useMemo } from "react";
-import PropTypes from "prop-types";
+import React from "react";
 import * as d3 from "d3";
 
 import Chart from "./Chart/Chart";
 import Bars from "./Chart/Bars";
 import Axis from "./Chart/Axis";
 import Legends from "./Chart/Legends";
-import { accessorPropsType } from "./Chart/utils";
 
 const fillColors = ["#FC766AFF", "#5B84B1FF"];
 const Histogram = ({
@@ -15,123 +13,115 @@ const Histogram = ({
   selectedValues,
   customWidth,
   customMarginWidth,
-}) =>
-  useMemo(() => {
-    const height = 600;
-    const width = 800;
-    const marginTop = customWidth > 1000 ? 100 : 0;
-    const marginBottom = 100;
-    const marginLeft = customWidth > 1000 ? 100 : 70;
-    const marginRight = customWidth > 1000 ? 100 : 25;
+}) => {
+  const height = 600;
+  const width = 800;
+  const marginTop = customWidth > 1000 ? 100 : 0;
+  const marginBottom = 100;
+  const marginLeft = customWidth > 1000 ? 100 : 70;
+  const marginRight = customWidth > 1000 ? 100 : 25;
 
-    let dimensions = {
-      height,
-      width,
-      marginTop,
-      marginBottom,
-      marginLeft,
-      marginRight,
-      boundedHeight: Math.max(height - marginTop - marginBottom, 0),
-      boundedWidth: Math.max(width - marginLeft - marginRight, 0),
-    };
+  let dimensions = {
+    height,
+    width,
+    marginTop,
+    marginBottom,
+    marginLeft,
+    marginRight,
+    boundedHeight: Math.max(height - marginTop - marginBottom, 0),
+    boundedWidth: Math.max(width - marginLeft - marginRight, 0),
+  };
 
-    const nullRemovedData = data.filter((area) => {
-      if (area.values.length > 0) {
-        return area.key;
-      }
-    });
-    const selectedAreas = selectedValues.map((d) => d.value);
-    const dateRange = d3.extent(
-      nullRemovedData.map((area) => new Date(area.key))
-    );
-    const allDates = data.map((area) => new Date(area.key));
-    const values = data.map((d) => d.values).flat();
-    const valuesRange = [
-      0,
-      d3.max(values.map((d) => d.dailyLabConfirmedCases)),
-    ];
+  const nullRemovedData = data.filter((area) => {
+    if (area.values.length > 0) {
+      return area.key;
+    }
+  });
+  const selectedAreas = selectedValues.map((d) => d.value);
+  const dateRange = d3.extent(
+    nullRemovedData.map((area) => new Date(area.key))
+  );
+  const allDates = data.map((area) => new Date(area.key));
+  const values = data.map((d) => d.values).flat();
+  const valuesRange = [0, d3.max(values.map((d) => d.dailyLabConfirmedCases))];
 
-    const xScale = d3
-      .scaleTime()
-      .domain(dateRange)
-      .range([0, dimensions.boundedWidth]);
+  const xScale = d3
+    .scaleTime()
+    .domain(dateRange)
+    .range([0, dimensions.boundedWidth]);
 
-    const yScale = d3
-      .scaleLinear()
-      .domain(valuesRange)
-      .range([dimensions.boundedHeight, 20]);
+  const yScale = d3
+    .scaleLinear()
+    .domain(valuesRange)
+    .range([dimensions.boundedHeight, 20]);
 
-    const xBand = d3
-      .scaleBand()
-      .domain(allDates)
-      .range([0, dimensions.boundedWidth])
-      .padding(0.2);
+  const xBand = d3
+    .scaleBand()
+    .domain(allDates)
+    .range([0, dimensions.boundedWidth])
+    .padding(0.2);
 
-    const xBand1 = d3
-      .scaleBand()
-      .domain(selectedAreas)
-      .range([0, xBand.bandwidth()]);
+  const xBand1 = d3
+    .scaleBand()
+    .domain(selectedAreas)
+    .range([0, xBand.bandwidth()]);
 
+<<<<<<< Updated upstream
     const barBandWidth = xBand.bandwidth();
+=======
+  const barBandWidth = xBand.bandwidth() > 100 ? 0 : xBand.bandwidth();
+>>>>>>> Stashed changes
 
-    const color = d3.scaleOrdinal().domain(selectedAreas).range(fillColors);
+  const color = d3.scaleOrdinal().domain(selectedAreas).range(fillColors);
 
-    const xAccessorScaled = (d) => xBand1(d.areaName);
-    const xAccessorGroupScaled = (d) => xScale(new Date(d.key));
-    const yAccessorScaled = (d) => yScale(d.dailyLabConfirmedCases);
-    const widthAccessorScaled = () => xBand1.bandwidth();
-    const heightAccessorScaled = (d) =>
-      dimensions.boundedHeight - yScale(d.dailyLabConfirmedCases);
-    const keyAccessor = (d, i) => i;
-    const colorAccessorScaled = (d) => color(d.areaName);
-    const colorScale = (d) => color(d);
+  const xAccessorScaled = (d) => xBand1(d.areaName);
+  const xAccessorGroupScaled = (d) => xScale(new Date(d.key));
+  const yAccessorScaled = (d) => yScale(d.dailyLabConfirmedCases);
+  const widthAccessorScaled = () => xBand1.bandwidth();
+  const heightAccessorScaled = (d) =>
+    dimensions.boundedHeight - yScale(d.dailyLabConfirmedCases);
+  const keyAccessor = (d, i) => i;
+  const colorAccessorScaled = (d) => color(d.areaName);
+  const colorScale = (d) => color(d);
 
-    return (
-      <div className="Histogram">
-        <Chart dimensions={dimensions}>
-          <Legends
-            data={selectedValues}
-            dimensions={dimensions}
-            scale={colorScale}
-          />
-          <Axis
-            dimensions={dimensions}
-            dimension="x"
-            scale={xScale}
-            label={label}
-            barBandWidth={barBandWidth}
-          />
-          <Axis
-            dimensions={dimensions}
-            dimension="y"
-            scale={yScale}
-            label="Count"
-          />
-          <Bars
-            data={data}
-            keyAccessor={keyAccessor}
-            xAccessorGroup={xAccessorGroupScaled}
-            xAccessor={xAccessorScaled}
-            yAccessor={yAccessorScaled}
-            widthAccessor={widthAccessorScaled}
-            heightAccessor={heightAccessorScaled}
-            fill={colorAccessorScaled}
-          />
-        </Chart>
-      </div>
-    );
-  }, [label, data, customMarginWidth, customWidth]);
-
-Histogram.propTypes = {
-  xAccessor: accessorPropsType,
-  yAccessor: accessorPropsType,
-  xLabel: PropTypes.string,
-  yLabel: PropTypes.string,
+  return (
+    <div className="Histogram">
+      <Chart dimensions={dimensions}>
+        <Legends
+          data={selectedValues}
+          dimensions={dimensions}
+          scale={colorScale}
+        />
+        <Axis
+          dimensions={dimensions}
+          dimension="x"
+          scale={xScale}
+          label={label}
+          barBandWidth={barBandWidth}
+        />
+        <Axis
+          dimensions={dimensions}
+          dimension="y"
+          scale={yScale}
+          label="Count"
+        />
+        <Bars
+          data={data}
+          keyAccessor={keyAccessor}
+          xAccessorGroup={xAccessorGroupScaled}
+          xAccessor={xAccessorScaled}
+          yAccessor={yAccessorScaled}
+          widthAccessor={widthAccessorScaled}
+          heightAccessor={heightAccessorScaled}
+          fill={colorAccessorScaled}
+        />
+      </Chart>
+    </div>
+  );
 };
 
-Histogram.defaultProps = {
-  xAccessor: (d) => d.x,
-  yAccessor: (d) => d.y,
-};
-
-export default Histogram;
+export default React.memo(Histogram, (previous, next) => {
+  if (next.selectedValues === null) {
+    return true;
+  } else if (previous !== next) return false;
+});
